2caf2cb570626b055f926d9d22a294eee4b9e3c2
Denys <denys.shtoma@knu.ua>
2025-05-23 10:54:57 +0300

    Add image detail view to display individual images

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Category
+from .models import Category, Image
 
 
 def gallery_view(request):
@@ -9,3 +9,10 @@ def gallery_view(request):
     """
     categories = Category.objects.prefetch_related("image_set")
     return render(request, "gallery.html", {"categories": categories})
+
+def image_detail(request, pk):
+    """
+    Показує одне конкретне зображення за його первинним ключем.
+    """
+    image = get_object_or_404(Image, pk=pk)
+    return render(request, "image_detail.html", {"image": image})
