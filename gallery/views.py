1429cabfac11c8cc4347e112629f9a3e222cb6ac
Denys <denys.shtoma@knu.ua>
2025-05-23 10:54:03 +0300

    Add gallery view to display categories with images

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+
+from .models import Category
+
+
+def gallery_view(request):
+    """
+    Показує усі категорії разом із їхніми зображеннями.
+    """
+    categories = Category.objects.prefetch_related("image_set")
+    return render(request, "gallery.html", {"categories": categories})
